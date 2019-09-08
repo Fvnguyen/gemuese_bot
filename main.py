82b008943cd2fc717f94356e9d49b09b5991917c
Fabian Nguyen <Fabian.Nguyen@united-it.net>
2019-09-08 12:36:18 +0200

    New unseasonal funcs and module structure

--- main.py
+++ main.py
@@ -9,39 +9,6 @@ import pandas as pd
 import veg_processes as vp
 from datetime import datetime
 
-# Gemuesefunktionen
-ledger = pd.read_csv("gemuese_full.csv",encoding = "utf-8", sep = ",")
-def seasonal():
-    current_month = datetime.today().month
-    seasonal = ledger.query("Month == "+str(current_month)).query("Seasonal == True")["gemuese"].tolist()
-    return seasonal
-
-def eng_seasonal():
-    current_month = datetime.today().month
-    seasonal = ledger.query("Month == "+str(current_month)).query("Seasonal == True")["vegetable"].tolist()
-    return seasonal
-
-def in_list():
-    master = ledger["gemuese"].tolist()
-    return master
-
-def suggestion():
-    suggestion = random.choice(seasonal())
-    return suggestion
-
-def look_up(veggie):
-    season_list = [x.lower() for x in seasonal()]
-    approval = vp.matching(veggie.lower(),season_list)
-    in_master = veggie.lower() in [x.lower() for x in in_list()]
-    if approval > 0.7:
-        approval = "Mmmmh, Saisonal...( ͡° ͜ʖ ͡°)"
-    else:
-        if in_master:
-            approval = "Igitt, importiert ಠ_ಠ"
-        else:
-            approval = "Ich kann das Gemüse in meiner Liste nicht finden. Ist es richtig geschrieben und auch ein heimisches Gemüse? Obst, Exotische Gemüse oder Getreide (wie Kartoffeln) sind in meiner Liste nicht enthalten."
-    return approval
-
 # Toke functions
 TELEGRAM_TOKEN = os.environ['gemuese_bot']
 
@@ -63,39 +30,19 @@ dispatcher.add_handler(start_handler)
 #Veggy function
 LOOKUP = range(1)
 def suggested_veggie(update, context):
-    context.bot.send_message(chat_id=update.message.chat_id, text="Warum kochst Du nicht etwas mit "+suggestion())
+    suggesion = vp.suggestion()[0]+' '+str(vp.suggestion()[1])
+    context.bot.send_message(chat_id=update.message.chat_id, text=suggestion)
 
 veggie_handler = CommandHandler('empfehlung', suggested_veggie)
 dispatcher.add_handler(veggie_handler)
 
 def veggie_list(update, context):
-    seasonal_list = ', '.join(seasonal())
-    context.bot.send_message(chat_id=update.message.chat_id, text="Diese Gemüse sind gerade saisonal:")
+    seasonal_list = ', '.join(vp.seasonal()[1])
+    context.bot.send_message(chat_id=update.message.chat_id, text="Diese Gemüsesorten sind diesen Monat in Saison:")
     context.bot.send_message(chat_id=update.message.chat_id, text=seasonal_list)
 
 veggieList_handler = CommandHandler('liste', veggie_list)
 dispatcher.add_handler(veggieList_handler)
-
-# def recipe(update, context):
-#     seasonal_list = ','.join(random.sample(eng_seasonal(),2))
-#     recipe = vp.getrecipe(seasonal_list)
-#     title = recipe['title']+':'
-#     summary = recipe['summary']
-#     context.bot.send_message(chat_id=update.message.chat_id, text=title)
-#     context.bot.send_message(chat_id=update.message.chat_id, text=summary, parse_mode=ParseMode.HTML)
-
-# recipe_handler = CommandHandler('rezept', recipe)
-# dispatcher.add_handler(recipe_handler)
-
-# def vrecipe(update, context):
-#     seasonal_list = ','.join(random.sample(eng_seasonal(),2))
-#     recipe = vp.veggyrecipe(seasonal_list)
-#     title = recipe['label']
-#     summary = '['+title+']'+'('+recipe['url']+')'
-#     context.bot.send_message(chat_id=update.message.chat_id, text=summary, parse_mode=ParseMode.MARKDOWN)
-
-# vrecipe_handler = CommandHandler('vrezept', vrecipe)
-# dispatcher.add_handler(vrecipe_handler)
 
 def start_lookup(update,context):
     update.message.reply_text('Welches Gemüse möchtest Du prüfen? (Antworte "cancel" zum abbrechen)')
@@ -108,7 +55,7 @@ def cancel(update,context):
 
 def veggie_lookup(update, context):
     test_veg = ''.join(update.message.text).strip()
-    approval = look_up(test_veg)
+    approval = vp.look_up(test_veg)
     update.message.reply_text(approval)
     return ConversationHandler.END
 
@@ -138,20 +85,20 @@ def diet(update, context):
     if answer == 'NONE':
         seasonal_list = ','.join(random.sample(eng_seasonal(),2))
         recipe = vp.getrecipe(seasonal_list)
-        title = recipe['label']
-        summary = '['+title+']'+'('+recipe['url']+')'
+        title = recipe[0]
+        summary = recipe[1]
         query.edit_message_text(text=summary,parse_mode=ParseMode.MARKDOWN)
     elif answer == 'vegetarian':
         seasonal_list = ','.join(random.sample(eng_seasonal(),2))
         recipe = vp.veggyrecipe(seasonal_list)
-        title = recipe['label']
-        summary = '['+title+']'+'('+recipe['url']+')'
+        title = recipe[0]
+        summary = recipe[1]
         query.edit_message_text(text=summary,parse_mode=ParseMode.MARKDOWN)
     else:
         seasonal_list = ','.join(random.sample(eng_seasonal(),2))
         recipe = vp.veganrecipe(seasonal_list)
-        title = recipe['label']
-        summary = '['+title+']'+'('+recipe['url']+')'
+        title = recipe[0]
+        summary = recipe[1]
         query.edit_message_text(text=summary,parse_mode=ParseMode.MARKDOWN)
         
 
--- veg_processes.py
+++ veg_processes.py
@@ -1,6 +1,36 @@
 import re
 import requests
 import os
+import random
+import time
+import os
+import pandas as pd
+from datetime import datetime
+from functools import wraps
+import pickle
+import redis
+
+# Function monitoring wrapper
+def stat_counter(id,function):
+    try:
+        filename = 'bot_stats'
+        data = pickle.loads(r.get(filename))
+        print("loaded stats")
+    except:
+        print("did not load stats")
+        data = []
+    entry = [{'id':id,'function':function,'time': datetime.now()}]
+    data.update(entry)
+    new_data = pickle.dumps(data)
+    r.set(filename,new_data)
+
+def stats(func):
+    @wraps(func)
+    def function_wrapper(update, context,*args, **kwargs):
+        stat_counter(update.effective_user.id,func.__name__)
+        return func(update, context,*args, **kwargs)
+    return function_wrapper
+
 
 #Calculates the normalized Levenshtein distance of 2 strings
 def levenshtein(s1, s2):
@@ -26,42 +56,92 @@ def matching(text,master_list):
     result = max(result_list)
     return result
 
-# Get's recipes based on ingredient_list
-# def getrecipe(ingredient_list):
-#     recipe_url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients"
-#     querystring = {"number":"1","ranking":"2","ignorePantry":"false","ingredients":ingredient_list}
-#     headers = {
-#         'x-rapidapi-host': "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-#         'x-rapidapi-key': "e8c26025abmsh045b21e5a969e8ap15fc5djsnbdc016803368"
-#         }
-
-#     response = requests.request("GET", recipe_url, headers=headers, params=querystring)
-#     id = response.json()[0]['id']
-#     summ_url = 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/' + str(id) + '/summary'
-#     response_summary  = requests.request("GET", summ_url, headers=headers)
-#     summary = response_summary.json()
-#     return summary
-  
-def veggyrecipe(ingredient_list):
+# recipe functions
+def veggyrecipe():
+    ingredient_list = ','.join(random.sample(eng_seasonal(),2))
+    uniseasonal = []
+    for x in eng_unseasonal():
+        uniseasonal.append('excluded='+x)
+    uniseasonal = '&'.join(uniseasonal)
     appid = '&app_id='+os.environ['EDAMAM_app']
     appapi = '&app_key='+os.environ['EDAMAM_api']
-    url = 'https://api.edamam.com/search?q='+ingredient_list+appid+appapi+'&health=vegetarian'
-    response = requests.request("GET", url)
+    url = 'https://api.edamam.com/search?q='+ingredient_list+'&'+uniseasonal+appid+appapi+'&health=vegetarian'
     recipe = response.json()['hits'][0]['recipe']
-    return recipe
+    summary = '['+recipe['label']+']'+'('+recipe['url']+')'
+    return 'Versuche es mal mit diesem leckeren Rezept',summary
 
-def veganrecipe(ingredient_list):
+def veganrecipe():
+    ingredient_list = ','.join(random.sample(eng_seasonal(),2))
+    uniseasonal = []
+    for x in eng_unseasonal():
+        uniseasonal.append('excluded='+x)
+    uniseasonal = '&'.join(uniseasonal)
     appid = '&app_id='+os.environ['EDAMAM_app']
     appapi = '&app_key='+os.environ['EDAMAM_api']
-    url = 'https://api.edamam.com/search?q='+ingredient_list+appid+appapi+'&health=vegan'
+    url = 'https://api.edamam.com/search?q='+ingredient_list+'&'+uniseasonal+appid+appapi+'&health=vegan'
     response = requests.request("GET", url)
     recipe = response.json()['hits'][0]['recipe']
-    return recipe
+    summary = '['+recipe['label']+']'+'('+recipe['url']+')'
+    return 'Versuche es mal mit diesem leckeren Rezept',summary
 
-def getrecipe(ingredient_list):
+def getrecipe():
+    ingredient_list = ','.join(random.sample(eng_seasonal(),2))
+    uniseasonal = []
+    for x in eng_unseasonal():
+        uniseasonal.append('excluded='+x)
+    uniseasonal = '&'.join(uniseasonal)
     appid = '&app_id='+os.environ['EDAMAM_app']
     appapi = '&app_key='+os.environ['EDAMAM_api']
-    url = 'https://api.edamam.com/search?q='+ingredient_list+appid+appapi
+    url = 'https://api.edamam.com/search?q='+ingredient_list+'&'+uniseasonal+appid+appapi
     response = requests.request("GET", url)
     recipe = response.json()['hits'][0]['recipe']
-    return recipe
+    summary = '['+recipe['label']+']'+'('+recipe['url']+')'
+    return 'Versuche es mal mit diesem leckeren Rezept',summary
+
+# Gemuesefunktionen
+
+def seasonal():
+    ledger = pd.read_csv("gemuese_full.csv",encoding = "utf-8", sep = ",")
+    current_month = datetime.today().month
+    seasonal = ledger.query("Month == "+str(current_month)).query("Seasonal == True")["gemuese"].tolist()
+    return 'Diese Gemüsesorten sind diesen Monat in Saison:',seasonal
+
+def unseasonal():
+    ledger = pd.read_csv("gemuese_full.csv",encoding = "utf-8", sep = ",")
+    current_month = datetime.today().month
+    unseasonal = ledger.query("Month == "+str(current_month)).query("Seasonal == False")["gemuese"].tolist()
+    return 'Diese Gemüsesorten sind diesen Monat NICHT in Saison:',unseasonal
+
+def eng_seasonal():
+    ledger = pd.read_csv("gemuese_full.csv",encoding = "utf-8", sep = ",")
+    current_month = datetime.today().month
+    seasonal = ledger.query("Month == "+str(current_month)).query("Seasonal == True")["vegetable"].tolist()
+    return seasonal
+
+def eng_unseasonal():
+    ledger = pd.read_csv("gemuese_full.csv",encoding = "utf-8", sep = ",")
+    current_month = datetime.today().month
+    unseasonal = ledger.query("Month == "+str(current_month)).query("Seasonal == False")["vegetable"].tolist()
+    return unseasonal
+
+def in_list():
+    ledger = pd.read_csv("gemuese_full.csv",encoding = "utf-8", sep = ",")
+    master = ledger["gemuese"].tolist()
+    return master
+
+def suggestion():
+    suggestion = random.sample(seasonal()[1],1)
+    return 'Warum kochst Du heute nicht etwas mit:',suggestion
+
+def look_up(veggie):
+    season_list = [x.lower() for x in seasonal()[1]]
+    approval = matching(veggie.lower(),season_list)
+    in_master = veggie.lower() in [x.lower() for x in in_list()]
+    if approval > 0.7:
+        approval = "Mmmmh, Saisonal...( ͡° ͜ʖ ͡°)"
+    else:
+        if in_master:
+            approval = "Igitt, importiert ಠ_ಠ"
+        else:
+            approval = "Ich kann das Gemüse in meiner Liste nicht finden. Ist es richtig geschrieben und auch ein heimisches Gemüse? Obst, Exotische Gemüse oder Getreide (wie Kartoffeln) sind in meiner Liste nicht enthalten."
+    return approval
